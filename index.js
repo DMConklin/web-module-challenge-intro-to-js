03d8c6cde12a0dc3fb4ffd0ab3f6b512882a18ad
DMConklin <dmconklin84@gmail.com>
2022-11-26 21:07:58 -0600

    completed task 5a

--- index.js
+++ index.js
@@ -226,8 +226,9 @@ Using the miles function below do the following:
 3. Return the number of miles
 */
 
-function miles(/*add your code here*/){
+function miles(/*add your code here*/kilometers){
   /*add your code here*/
+  return kilometers * 0.621371
 }
 
 
